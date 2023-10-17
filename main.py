7774872d6c9e734131d1e22d5ca7a2b0126f5b05
Fabrice Fourel <90900297+fabricefo@users.noreply.github.com>
2023-10-17 17:37:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from website import create_app
 
+/*
+IMPORTANT : note that the version of Werkzeug must be < 3.0.0
+For this project, we use the 2.2.0 version - see requirements.txt file
+*/
+
 app = create_app()
 
 if __name__ == '__main__':
